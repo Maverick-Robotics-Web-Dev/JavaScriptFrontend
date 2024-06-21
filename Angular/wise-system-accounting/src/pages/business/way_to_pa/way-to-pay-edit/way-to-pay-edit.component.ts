b99dacc0e418dfb17a272e30201532908b833138
Maverick7690 <142626480+Maverick-Robotics-Web-Dev@users.noreply.github.com>
2024-06-20 22:05:45 -0500

    Here we go

--- Angular/wise-system-accounting/src/pages/business/way_to_pa/way-to-pay-edit/way-to-pay-edit.component.ts
+++ Angular/wise-system-accounting/src/pages/business/way_to_pa/way-to-pay-edit/way-to-pay-edit.component.ts
@@ -1,5 +1,5 @@
-import { Component, Input, inject } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component, Input, OnInit, inject } from '@angular/core';
+import { EMPTY, Observable, catchError } from 'rxjs';
 import { WaytoPayRModel } from '../../../../models/business';
 import { WayToPayService } from '../../../../services/business';
 import { Router } from 'express';
@@ -12,10 +12,19 @@ import { HttpErrorResponse } from '@angular/common/http';
   templateUrl: './way-to-pay-edit.component.html',
   styleUrl: './way-to-pay-edit.component.css',
 })
-export class WayToPayEditComponent {
+export class WayToPayEditComponent implements OnInit {
   @Input() id!: string;
   public waytopay$!: Observable<WaytoPayRModel>;
   private _apirestService: WayToPayService = inject(WayToPayService);
   private _router: Router = inject(Router);
   public httpError!: HttpErrorResponse;
+
+  ngOnInit(): void {
+    this.waytopay$ = this._apirestService.retrieve(this.id).pipe(
+      catchError((error: HttpErrorResponse) => {
+        this.httpError = error;
+        return EMPTY;
+      })
+    );
+  }
 }
